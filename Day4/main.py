ccb8b2f89ecbf90eaca87abce831b5cc2556a139
Basilisa1008 <104135280+Basilisa1008@users.noreply.github.com>
2023-07-05 19:48:41 +0300

    day4 task

---
+++ Day4/main.py
@@ -0,0 +1,8 @@
+
+print("""Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!""")
+Name=input("What is your name? ")
+Enemy=("What is your worst enemy’s name? ")
+Power=input("What is your superpower? ")
+stayAt=input("Where do you live? ")
+Food=input("What is your favorite food? ")
+print("""Hello {Name}! Your ability to {Power} will make sure you never have to look at {Enemy} again. Go eat {Food} as you walk down the streets of {stayAt} and use {Power} for good and not evil!""")
