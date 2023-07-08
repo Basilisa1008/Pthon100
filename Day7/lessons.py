0feb7c0b7c028b31ab79f1a1d3a50462a1a53336
Basilisa1008 <104135280+Basilisa1008@users.noreply.github.com>
2023-07-08 20:32:27 +0300

    day7(nested if)

---
+++ Day7/lessons.py
@@ -0,0 +1,80 @@
+
+# Common errors in nested if,  looking at proper indentication of the if statements
+
+tvShow = input("What is your favourite tv show? ")
+if tvShow == "peppa pig":
+  print("Ugh, why?")
+  faveCharacter = input("Who is your favourite character? ")
+  if faveCharacter == "daddy pig":
+    print("Right answer")
+  else:
+    print("Nah, Daddy Pig's the greatest")
+elif tvShow == "paw patrol":
+  print("Aww, sad times")
+else:
+  print("Yeah, that's cool and all…")
+
+#   before the fix
+# Fix my code
+order = input(What would you like to order: pizza or hamburger? ")
+if order = "hamburger":
+print("Thank you.")
+  cheese = input("Do you want cheese?")
+  if cheese == "yes":
+  print("You got it.")
+else: 
+    print("No cheese it is.")
+elif order == pizza:
+  print("Pizza coming up.")
+  toppings = input("Do you want pepperoni on that?")
+  if toppings = "yes"
+    print("We will add pepperoni.")
+else:
+  print"Your pizza will not have pepperoni.")
+
+#   after the fix
+# Fix my code
+order = input("What would you like to order: pizza or hamburger? ")
+if order == "hamburger":
+  print("Thank you.")
+  cheese = input("Do you want cheese?")
+  if cheese == "yes":
+    print("You got it.")
+  else: 
+    print("No cheese it is.")
+elif order == "pizza":
+    print("Pizza coming up.")
+    toppings = input("Do you want pepperoni on that?")
+if toppings == "yes":
+    print("We will add pepperoni.")
+else:
+  print("Your pizza will not have pepperoni.")
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day7/main.py
@@ -0,0 +1,23 @@
+# Fake Fan Question Generator
+
+print("LET US SEE HOW WELL YOU KNOW THIS CHARACTERS !!!!!!")
+print("*****************************************************")
+print()
+
+tvShow=input("What is your all time favourite anime show > ")
+captain=input("What is the name of the captain with initial D > ")
+print()
+if tvShow=="One Piece" and captain=="Monkey D Luffy":
+  print("Oh well you are a Straw's Hat Fan !")
+  if tvShow=="One Piece" and captain=="Tafalga D Law":
+      print("The Straw Hat's are the best, anyways you are Team Law")
+  else:
+        print("Never heard of One Piece before?")
+
+if tvShow=="Attack On Tittan" and captain=="Levi Arckamen":
+          print("The commander is still mad at Eren Eager")
+
+else:
+    print("Opps! you are not fan of anime")
+
+print("+++++++++++++++++++++++++++++++++++++")
